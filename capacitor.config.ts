44490c9d2e50e8c87a85367fac8099cca344fc6b
michelle2444444 <almeandrapark@gmail.com>
2025-05-22 14:35:34 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rickmortyApp',
+  webDir: 'www'
+};
+
+export default config;
